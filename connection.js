808aef5b40ab805b8d21a2ba9b19dc3a8ebc8709
or abdillh <hans.abdillh05@gmail.com>
2022-02-16 23:40:23 +0800

    Create connection to mysql

---
+++ connection.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const mysql = require('mysql')
+require('dotenv').config()
+
+const conn = mysql.createConnection({
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_NAME
+})
+
+conn.connect( err => {
+	if (err) throw err
+	console.log('MySQL connected !!')
+})
+
+module.exports = conn
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 'use strict'
 
 const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const conn = require('./connection.js')
 
 const app = express()
 const PORT = process.env.PORT || 8080
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors())
+
 app.listen(PORT, err => {
 	if (err) throw err
+	console.log(`Start time ${new Date().toLocaleString('id')}`)
 	console.log(`Server running at PORT ${PORT} !!`)
 })
