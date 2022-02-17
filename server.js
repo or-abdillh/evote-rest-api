d41ce5f904feec90f3991cad3ff0d590e9185322
or abdillh <hans.abdillh05@gmail.com>
2022-02-17 12:27:10 +0800

    Create middeware for logger request API

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const conn = require('./connection.js')
 const router = require('./router')
+const logger = require('./middleware/logger.js')
 
 const app = express()
 const PORT = process.env.PORT || 8080
@@ -13,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
 
+app.use(logger)
 router(app)
 
 app.listen(PORT, err => {
